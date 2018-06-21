7e4b8877d9740b2277d7bbfd26c861fe713cab75
Alex Berg <chexxor@gmail.com>
2018-06-21 13:00:38 -0500

    Progress! Stuck on overlapping TCIs.

--- src/DOM/Tagless/Class.purs
+++ src/DOM/Tagless/Class.purs
@@ -4,12 +4,13 @@ import Prelude
 
 import Data.Nullable (Nullable)
 import Effect (Effect)
+import Unsafe.Coerce (unsafeCoerce)
 
 
-class WindowTC win where
+class WindowTC m document where
 
   -- Returns a reference to the document contained in the window.
-  document :: forall document. DocumentTC win document => win document
+  document :: DocumentTC m document => m document
 
 
 -- A String containing one or more selectors to match.
@@ -21,7 +22,7 @@ newtype TagName = TagName String
 --   point into the web page's content, which is the DOM tree.
 -- https://developer.mozilla.org/en-US/docs/Web/API/Document
 -- A `Document` is also a `Node`.
-class (NodeTC m document) <= DocumentTC m document where
+class (NodeTC m document) <= DocumentTC m document | m -> document where
 
   -- Create the HTML element specified by `TagName`.
   createElement :: forall element. ElementTC m element =>
@@ -40,7 +41,7 @@ class (NodeTC m document) <= DocumentTC m document where
 -- https://developer.mozilla.org/en-US/docs/Web/API/Element
 -- An `Element` is also a `Node`.
 -- For now, will presume `m` contains a single `Document`.
-class (NodeTC m element) <= ElementTC m element where
+class (NodeTC m element) <= ElementTC m element | m -> element where
 
   -- Sets the value of an attribute on the specified element.
   --   If the attribute already exists, the value is updated;
@@ -59,7 +60,7 @@ class (NodeTC m element) <= ElementTC m element where
 --   It allows those types to be treated similarly; for example, inheriting the same set of methods,
 --   or being tested in the same way.
 -- https://developer.mozilla.org/en-US/docs/Web/API/Node
-class NodeTC m node where
+class NodeTC m node | m -> node where
 
   -- Appends the first argument to the child node list of the second.
   appendChild :: node -> node -> m Unit
@@ -82,34 +83,30 @@ foreign import data Node :: Type
 foreign import documentImpl :: Effect Document
 foreign import createElementImpl :: String -> Document -> Effect Element
 foreign import querySelectorImpl :: String -> Effect (Nullable Element)
-foreign import getAttributeImpl :: String -> Element -> Effect String
+foreign import getAttributeImpl :: String -> Element -> Effect (Nullable String)
 foreign import setAttributeImpl :: String -> String -> Element -> Effect Unit
 foreign import appendChildImpl :: Node -> Node -> Effect Unit
 
 
--- ERROR:
--- Could not match type    Document  with type    document0
-instance windowEffect :: WindowTC Effect where
+instance windowEffect :: WindowTC Effect Document where
 
-  -- document :: forall document. DocumentTC win document => win document
-  
-  document :: DocumentTC Effect Document => Effect Document
-
-  -- document :: forall document. DocumentTC Effect document => Effect document
-  -- document :: Effect Document
+  document :: Effect Document
   document = documentImpl
 
 
-instance documentEffect :: DocumentTC Effect Document where
+instance documentEffectDocument :: DocumentTC Effect Document where
+
+  -- createElement :: forall element. ElementTC m element =>
+  --    TagName -> document -> m element
 
   -- Create the HTML element specified by `TagName`.
-  createElement :: forall element. ElementTC element =>
+  createElement :: forall element. ElementTC Effect element =>
       TagName -> Document -> Effect element
   createElement (TagName name) doc = createElementImpl name doc
 
   -- Returns the first Element within the document that matches the specified selector,
   --   or group of selectors.
-  querySelector :: forall element. ElementTC element =>
+  querySelector :: forall element. ElementTC Effect element =>
       Selector -> Document -> Effect (Nullable element)
   querySelector (Selector s) doc = querySelectorImpl s doc
 
@@ -126,7 +123,7 @@ instance elementEffect :: ElementTC Effect Element where
   setAttribute :: String -> String -> Element -> Effect Unit
   setAttribute name value elem = setAttributeImpl name value elem
 
-  getAttribute :: String -> Element -> Effect String
+  getAttribute :: String -> Element -> Effect (Nullable String)
   getAttribute attrName elem = getAttributeImpl attrName elem
 
 
@@ -134,9 +131,28 @@ instance elementEffect :: ElementTC Effect Element where
 --   It allows those types to be treated similarly; for example, inheriting the same set of methods,
 --   or being tested in the same way.
 -- https://developer.mozilla.org/en-US/docs/Web/API/Node
-instance nodeEffect :: NodeTC Effect Node where
+instance nodeEffectNode :: NodeTC Effect Node where
 
   -- Appends the first argument to the child node list of the second.
   appendChild :: Node -> Node -> Effect Unit
-  appendChild n1 n2 = appendChildImpl n1 n2
+  appendChild n hostNode = appendChildImpl n hostNode
 
+-- !!! ERROR:
+-- Overlapping type class instances found for
+--     DOM.Tagless.Class.NodeTC Effect
+--                              Element
+--   The following instances were found:
+--     DOM.Tagless.Class.nodeEffectNode
+--     DOM.Tagless.Class.nodeEffectElement
+-- in type class instance
+--   DOM.Tagless.Class.NodeTC Effect
+--                            Element
+
+instance nodeEffectElement :: NodeTC Effect Element where
+  
+  appendChild :: Element -> Element -> Effect Unit
+  appendChild e hostElement = (unsafeCoerce appendChildImpl) e hostElement
+
+instance nodeEffectDocument :: NodeTC Effect Document where
+  appendChild :: Document -> Document -> Effect Unit
+  appendChild d hostDocument = pure unit
